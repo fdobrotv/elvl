26764435ec48f45aa41c80b10e48e04ea5384ff8
Dobrotvorsky Fedor Lvovich <fdobrotv@gradoservice.ru>
2020-02-26 23:26:39 +0600

    [RF] QuoteInValidator.kt hardened, Kotlin JPA plugin used

--- src/main/kotlin/com/fdobrotv/elvl/impl/validator/QuoteInValidator.kt
+++ src/main/kotlin/com/fdobrotv/elvl/impl/validator/QuoteInValidator.kt
@@ -7,7 +7,8 @@ import org.springframework.validation.Validator
 class QuoteInValidator : Validator {
     override fun validate(target: Any, errors: Errors) {
         val quoteIn: QuoteIn = target as QuoteIn
-        if (quoteIn.bid >= quoteIn.ask) {
+
+        if (quoteIn.bid != null && quoteIn.bid >= quoteIn.ask) {
             errors.rejectValue("bid", "bid.must.be.less.than.ask");
         }
     }
