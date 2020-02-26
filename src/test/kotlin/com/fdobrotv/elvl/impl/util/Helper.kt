d8ba03eaaf867b83369d7e6b34c34782599d628c
Dobrotvorsky Fedor Lvovich <fdobrotv@gradoservice.ru>
2020-02-26 22:32:23 +0600

    [RF] Helper.kt improved

--- src/test/kotlin/com/fdobrotv/elvl/impl/util/Helper.kt
+++ src/test/kotlin/com/fdobrotv/elvl/impl/util/Helper.kt
@@ -3,4 +3,4 @@ package com.fdobrotv.elvl.impl.util
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-fun bigDecimal(double: Double) = BigDecimal(double).setScale(2, RoundingMode.HALF_EVEN)
+fun bigDecimal(int: Number) = BigDecimal(int.toDouble()).setScale(2, RoundingMode.HALF_EVEN)
